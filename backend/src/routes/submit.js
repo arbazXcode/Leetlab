308e49d5c6627337bbeea2c10d28a1a7830ff6b9
arbazXcode <arbazjmi79@gmail.com>
2025-11-29 01:52:02 +0530

    downgrade redis to v4 to fix deploy

--- backend/src/routes/submit.js
+++ backend/src/routes/submit.js
@@ -1,11 +1,11 @@
 const express = require("express")
 const userMiddleware = require("../middleware/userMiddleware.js")
 const { submitCode, runcode } = require("../controllers/userSubmission.js")
-const submissionRateLimiter = require("../middleware/rateLimiter.js");
+// const submissionRateLimiter = require("../middleware/rateLimiter.js");
 
 const submitRouter = express.Router()
 
-submitRouter.post("/submit/:id", userMiddleware, submissionRateLimiter, submitCode)
+submitRouter.post("/submit/:id", userMiddleware, submitCode)
 submitRouter.post("/run/:id", userMiddleware, runcode)
 
 module.exports = submitRouter
