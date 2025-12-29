fc72c39de3d5492ad331605fd92f72ce480eb33d
arbazXcode <jmi.arbaz1@gmail.com>
2025-12-30 01:17:40 +0530

    fixed node mongoose mongodb versions

--- backend/src/routes/submit.js
+++ backend/src/routes/submit.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const userMiddleware = require("../middleware/userMiddleware.js")
 const { submitCode, runcode } = require("../controllers/userSubmission.js")
-// const submissionRateLimiter = require("../middleware/rateLimiter.js");
+const submissionRateLimiter = require("../middleware/rateLimiter.js");
 
 const submitRouter = express.Router()
 
